e8b7df45a298b66e8ec0f9fea669065715cbf00d
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-11 14:37:01 +0100

    feat: add box style

--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@ for (let i = 1; i <= 100; i++) {
 
   if (i % 15 == 0) {
     boxElement.innerText = "FizzBuzz";
+    boxElement.classList.add("boxFizzBuzz");
   } else if (i % 5 == 0) {
     boxElement.innerText = "Buzz";
+    boxElement.classList.add("boxBuzz");
   } else if (i % 3 == 0) {
     boxElement.innerText = "Fizz";
+    boxElement.classList.add("boxFizz");
   } else {
     boxElement.innerText = i;
   }
